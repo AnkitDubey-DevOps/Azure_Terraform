8929252bb3795e3c5af484bc225087538cbc36c9
Ankit Dubey <ak9511643861@gmail.com>
2026-05-11 12:05:49 +0530

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,176 @@
+# =========================
+# Resource Group
+# =========================
+
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-rg"
+  location = "West US 2"
+}
+
+# =========================
+# Virtual Network
+# =========================
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "terraform-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# =========================
+# Subnet
+# =========================
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "terraform-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  depends_on = [
+    azurerm_virtual_network.vnet
+  ]
+}
+
+# =========================
+# Public IP
+# =========================
+
+resource "azurerm_public_ip" "pip" {
+  name                = "terraform-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
+
+# =========================
+# Network Security Group
+# =========================
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "terraform-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# =========================
+# Allow SSH
+# =========================
+
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "AllowSSH"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+
+  depends_on = [
+    azurerm_network_security_group.nsg
+  ]
+}
+
+# =========================
+# Allow HTTP
+# =========================
+
+resource "azurerm_network_security_rule" "http" {
+  name                        = "AllowHTTP"
+  priority                    = 1002
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+
+  depends_on = [
+    azurerm_network_security_group.nsg
+  ]
+}
+
+# =========================
+# Network Interface
+# =========================
+
+resource "azurerm_network_interface" "nic" {
+  name                = "terraform-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+
+  depends_on = [
+    azurerm_subnet.subnet,
+    azurerm_public_ip.pip
+  ]
+}
+
+# =========================
+# NSG Association
+# =========================
+
+resource "azurerm_network_interface_security_group_association" "assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# =========================
+# Linux Virtual Machine
+# =========================
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "terraform-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  # Stable VM size for Azure free/student subscriptions
+  size = "Standard_DS1_v2"
+
+  admin_username = "azureuser"
+
+  disable_password_authentication = true
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("C:\\Users\\ankit\\.ssh\\id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  depends_on = [
+    azurerm_network_interface_security_group_association.assoc
+  ]
+}
