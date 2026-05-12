6050a43859bd398ee0581e7e4d9f7548eaaeb09c
Ankit Dubey <ak9511643861@gmail.com>
2026-05-12 09:15:18 +0530

    Updated Code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,14 @@
 # =========================
 # Resource Group
 # =========================
-
 resource "azurerm_resource_group" "rg" {
   name     = "terraform-rg"
-  location = "West US 2"
+  location = "Central US"
 }
 
 # =========================
-# Virtual Network
+# Virtual Network & Subnet
 # =========================
-
 resource "azurerm_virtual_network" "vnet" {
   name                = "terraform-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -18,94 +16,60 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-# =========================
-# Subnet
-# =========================
-
 resource "azurerm_subnet" "subnet" {
   name                 = "terraform-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-
-  depends_on = [
-    azurerm_virtual_network.vnet
-  ]
 }
 
 # =========================
 # Public IP
 # =========================
-
 resource "azurerm_public_ip" "pip" {
   name                = "terraform-public-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  allocation_method = "Static"
-  sku               = "Standard"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 # =========================
-# Network Security Group
+# Network Security Group & Rules
 # =========================
-
 resource "azurerm_network_security_group" "nsg" {
   name                = "terraform-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-}
 
-# =========================
-# Allow SSH
-# =========================
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-resource "azurerm_network_security_rule" "ssh" {
-  name                        = "AllowSSH"
-  priority                    = 1001
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-
-  depends_on = [
-    azurerm_network_security_group.nsg
-  ]
-}
-
-# =========================
-# Allow HTTP
-# =========================
-
-resource "azurerm_network_security_rule" "http" {
-  name                        = "AllowHTTP"
-  priority                    = 1002
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "80"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-
-  depends_on = [
-    azurerm_network_security_group.nsg
-  ]
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # =========================
 # Network Interface
 # =========================
-
 resource "azurerm_network_interface" "nic" {
   name                = "terraform-nic"
   location            = azurerm_resource_group.rg.location
@@ -117,17 +81,9 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
   }
-
-  depends_on = [
-    azurerm_subnet.subnet,
-    azurerm_public_ip.pip
-  ]
 }
 
-# =========================
-# NSG Association
-# =========================
-
+# Associate NSG to NIC
 resource "azurerm_network_interface_security_group_association" "assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
@@ -136,18 +92,12 @@ resource "azurerm_network_interface_security_group_association" "assoc" {
 # =========================
 # Linux Virtual Machine
 # =========================
-
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "terraform-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-
-  # Stable VM size for Azure free/student subscriptions
-  size = "Standard_DS1_v2"
-
-  admin_username = "azureuser"
-
-  disable_password_authentication = true
+  size                = "Standard_D2s_v3"
+  admin_username      = "azureuser"
 
   network_interface_ids = [
     azurerm_network_interface.nic.id
@@ -155,7 +105,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("C:\\Users\\ankit\\.ssh\\id_rsa.pub")
+    # Using forward slashes for the path to avoid escape character issues
+    public_key = file("C:/Users/ankit/.ssh/id_rsa.pub")
   }
 
   os_disk {
@@ -170,7 +121,6 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 
-  depends_on = [
-    azurerm_network_interface_security_group_association.assoc
-  ]
+  # Ensure the NIC is associated with the NSG before creating the VM
+  depends_on = [azurerm_network_interface_security_group_association.assoc]
 }
